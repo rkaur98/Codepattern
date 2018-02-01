06a4354934f9cf2973cd3551f27ae4ca7bbcd9e2
rkaur98 <rupinderbal.rupi@gmail.com>
2018-02-01 10:27:28 -0800

    Update profile.php

--- Coder/profile.php
+++ Coder/profile.php
@@ -15,41 +15,45 @@
 
 </head>
 <body>
+    <script>
+      a = window.location.hash.substring(1);
+      console.log(a);
+      document.getElementById('uid').value = a;
+    </script>  
+    <form action="" method="post">
 
+        <input type="text" name="uid" id="uid">
+
+        <button type="submit" name="submit">Start</button>
+	 
+    </form>
 	<header>
 		<h1>Code Pattern</h1>
 	</header>
 
 	<main class="index">
-    <a class="ref" href="level1.php">Level1</a>
-    <a class="ref" href="level2.php">Level2</a>
-    
-<!--     <script>
-      a = window.location.hash.substring(1);
-      console.log(a);
-      document.getElementById('uid').innerHTML = a;
-    </script>   -->
-	<?php
-		$url = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-		echo $url; // Outputs: Full URL
-		if( strpos( $url, "#" ) === false ) echo "NO HASH !";
-   		else echo "HASH IS: #".explode( "#", $url )[1]; 
-// 	$url=parse_url("http://domain.com/site/gallery/1#photo45 ");
-// 	echo $url["fragment"];
-// 	  $sql = "SELECT * FROM users WHERE ID = ".$_POST["uname"]." ";
-//           $result = sqlsrv_query($conn, $sql);
-//           if ($result == FALSE)
-//           echo (sqlsrv_errors());
-		  
-//           while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
-//            echo ($row['username'] . PHP_EOL);
+	    <a class="ref" href="level1.php">Level1</a>
+	    <a class="ref" href="level2.php">Level2</a>
 		
-//           }
-	  
-//           sqlsrv_free_stmt($result);
     
-	 ?>
-
+    
+	<?php
+	 if(isset($_POST['submit']))
+      	  {
+          $sql = "SELECT * FROM users WHERE ID = ".$_POST["uid"]." ";
+          $result = sqlsrv_query($conn, $sql);
+          if ($result == FALSE)
+          echo (sqlsrv_errors());
+		  
+          while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
+           echo ($row['username'] . PHP_EOL);
+		
+          }
+	  
+          sqlsrv_free_stmt($result);
+     
+        }
+	    ?>
 	</main>
 
 </body>
